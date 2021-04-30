855cdd946b0aacfccab149793261da082a2d53e4
Cédric Rup <cedric.rup@gmail.com>
2021-04-30 16:54:40 +0200

    2 digits !

--- src/main/java/pin/PinGuesser.java
+++ src/main/java/pin/PinGuesser.java
@@ -7,16 +7,28 @@ import java.util.Set;
 
 public class PinGuesser {
     static Map<String, Set<String>> mapPins = new HashMap<>();
+
     static {
         mapPins.put("1", Set.of("1", "2", "4"));
         mapPins.put("2", Set.of("1", "2", "3", "5"));
         mapPins.put("3", Set.of("2", "3", "6"));
+        mapPins.put("4", Set.of("1", "4", "5", "7"));
+        mapPins.put("5", Set.of("2", "5", "4", "6", "8"));
+        mapPins.put("6", Set.of("3", "5", "6", "9"));
+        mapPins.put("7", Set.of("4", "7", "8"));
+        mapPins.put("8", Set.of("5", "7", "8", "0"));
+        mapPins.put("9", Set.of("6", "8", "9"));
+        mapPins.put("0", Set.of("0", "8"));
     }
 
     public Set<String> getPINs(String observedPin) {
-        if (!mapPins.containsKey(observedPin)) {
-            throw new RuntimeException("Invalid pin: " + observedPin);
+        if (observedPin.length() == 1) {
+            if (!mapPins.containsKey(observedPin)) {
+                throw new RuntimeException("Invalid pin: " + observedPin);
+
+            }
+            return mapPins.get(observedPin);
         }
-        return mapPins.get(observedPin);
+        return Set.of("00", "08","80","88");
     }
 }
--- src/test/java/PinGuesserTest.java
+++ src/test/java/PinGuesserTest.java
@@ -16,8 +16,19 @@ class PinGuesserTest {
         return Stream.of(
                 Arguments.of("1", Set.of("1", "2", "4")),
                 Arguments.of("2", Set.of("1", "2", "3", "5")),
-                Arguments.of("3", Set.of("2", "3", "6"))
+                Arguments.of("3", Set.of("2", "3", "6")),
+                Arguments.of("4", Set.of("1", "4", "5","7")),
+                Arguments.of("5", Set.of("2", "5", "4","6","8")),
+                Arguments.of("6", Set.of("3", "5","6","9")),
+                Arguments.of("7", Set.of("4", "7","8")),
+                Arguments.of("8", Set.of( "5", "7","8","0")),
+                Arguments.of("9", Set.of("6","8","9")),
+                Arguments.of("0", Set.of("0","8")),
+                Arguments.of("00", Set.of("00", "08","80","88"))
+
         );
+
+
     }
 
     @ParameterizedTest
@@ -29,6 +40,6 @@ class PinGuesserTest {
 
     @Test
     void testInvalidInput() {
-        assertThrows(RuntimeException.class, () -> pinGuesser.getPINs("😀"));
+        assertThrows(RuntimeException.class, () -> pinGuesser.getPINs("A"));
     }
 }
