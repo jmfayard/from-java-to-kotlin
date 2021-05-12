883db94147280f2de9f6f0df2fa23347beb5e101
Jean-Michel Fayard <jmfayard@gmail.com>
2021-05-12 09:56:06 +0200

    Use the stlib for sets

--- src/test/java/PinGuesserTest.kt
+++ src/test/java/PinGuesserTest.kt
@@ -26,7 +26,7 @@ internal class PinGuesserTest {
     }
 
     fun testCombineSolutions() {
-        val actual = combineSolutions(java.util.Set.of("12", "34"), java.util.Set.of("8", "0"))
+        val actual = combineSolutions(setOf("12", "34"), setOf("8", "0"))
         val expected = setOf("128", "120", "348", "340")
         assertEquals(actual, expected)
     }
@@ -38,19 +38,19 @@ internal class PinGuesserTest {
 
         @JvmStatic fun testSingleDigitParameters(): Stream<Arguments> {
             return Stream.of(
-                Arguments.of("1", java.util.Set.of("1", "2", "4")),
-                Arguments.of("2", java.util.Set.of("1", "2", "3", "5")),
-                Arguments.of("3", java.util.Set.of("2", "3", "6")),
-                Arguments.of("4", java.util.Set.of("1", "4", "5", "7")),
-                Arguments.of("5", java.util.Set.of("2", "5", "4", "6", "8")),
-                Arguments.of("6", java.util.Set.of("3", "5", "6", "9")),
-                Arguments.of("7", java.util.Set.of("4", "7", "8")),
-                Arguments.of("8", java.util.Set.of("5", "7", "8", "0")),
-                Arguments.of("9", java.util.Set.of("6", "8", "9")),
-                Arguments.of("0", java.util.Set.of("0", "8")),
-                Arguments.of("00", java.util.Set.of("00", "08", "80", "88")),
-                Arguments.of("09", java.util.Set.of("06", "08", "09", "86", "88", "89")),
-                Arguments.of("", java.util.Set.of("")),
+                Arguments.of("1", setOf("1", "2", "4")),
+                Arguments.of("2", setOf("1", "2", "3", "5")),
+                Arguments.of("3", setOf("2", "3", "6")),
+                Arguments.of("4", setOf("1", "4", "5", "7")),
+                Arguments.of("5", setOf("2", "5", "4", "6", "8")),
+                Arguments.of("6", setOf("3", "5", "6", "9")),
+                Arguments.of("7", setOf("4", "7", "8")),
+                Arguments.of("8", setOf("5", "7", "8", "0")),
+                Arguments.of("9", setOf("6", "8", "9")),
+                Arguments.of("0", setOf("0", "8")),
+                Arguments.of("00", setOf("00", "08", "80", "88")),
+                Arguments.of("09", setOf("06", "08", "09", "86", "88", "89")),
+                Arguments.of("", setOf("")),
                 Arguments.of("090", setOf("088, 880, 068, 860, 090, 898, 080, 888, 060, 868, 098, 890")),
                 Arguments.of(
                     "0900",
