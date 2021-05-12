08f5f3d1d5f84a8746a671e3828c18d3d703ae06
Jean-Michel Fayard <jmfayard@gmail.com>
2021-05-12 09:36:50 +0200

    Convert Java File to Kotlin File

--- src/main/java/pin/PinGuesser.kt
+++ src/main/java/pin/PinGuesser.kt
@@ -1,50 +1,50 @@
-package pin;
+package pin
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
+import java.util.HashMap
+import pin.PinGuesser
+import java.lang.RuntimeException
+import java.util.stream.Collectors
 
+class PinGuesser {
+    companion object {
+        var mapPins: MutableMap<String, Set<String>> = HashMap()
 
-public class PinGuesser {
-    static Map<String, Set<String>> mapPins = new HashMap<>();
-
-    static {
-        mapPins.put("1", Set.of("1", "2", "4"));
-        mapPins.put("2", Set.of("1", "2", "3", "5"));
-        mapPins.put("3", Set.of("2", "3", "6"));
-        mapPins.put("4", Set.of("1", "4", "5", "7"));
-        mapPins.put("5", Set.of("2", "5", "4", "6", "8"));
-        mapPins.put("6", Set.of("3", "5", "6", "9"));
-        mapPins.put("7", Set.of("4", "7", "8"));
-        mapPins.put("8", Set.of("5", "7", "8", "0"));
-        mapPins.put("9", Set.of("6", "8", "9"));
-        mapPins.put("0", Set.of("0", "8"));
+        init {
+            mapPins["1"] = java.util.Set.of("1", "2", "4")
+            mapPins["2"] = java.util.Set.of("1", "2", "3", "5")
+            mapPins["3"] = java.util.Set.of("2", "3", "6")
+            mapPins["4"] = java.util.Set.of("1", "4", "5", "7")
+            mapPins["5"] = java.util.Set.of("2", "5", "4", "6", "8")
+            mapPins["6"] = java.util.Set.of("3", "5", "6", "9")
+            mapPins["7"] = java.util.Set.of("4", "7", "8")
+            mapPins["8"] = java.util.Set.of("5", "7", "8", "0")
+            mapPins["9"] = java.util.Set.of("6", "8", "9")
+            mapPins["0"] = java.util.Set.of("0", "8")
+        }
     }
 
-    public Set<String> getPINs(String observedPin) {
-        for (char c : observedPin.toCharArray()) {
-            if (!mapPins.containsKey(c + ""))
-                throw new RuntimeException("PIN " + observedPin + " contains invalid character " + c);
+    fun getPINs(observedPin: String): Set<String> {
+        for (c in observedPin.toCharArray()) {
+            if (!mapPins.containsKey(c.toString() + "")) throw RuntimeException("PIN $observedPin contains invalid character $c")
         }
-
         if (observedPin.isEmpty()) {
-            return Set.of();
+            return java.util.Set.of()
         }
-        Set<String> pins1 = mapPins.get(observedPin.charAt(0) + "");
-        if (observedPin.length() == 1) {
-            return pins1;
+        val pins1 = mapPins[observedPin[0].toString() + ""]!!
+        return if (observedPin.length == 1) {
+            pins1
         } else {
-            return combineSolutions(pins1, getPINs(observedPin.substring(1)));
+            combineSolutions(pins1, getPINs(observedPin.substring(1)))
         }
     }
 
-    public Set<String> combineSolutions(Set<String> pins1, Set<String> pins2) {
+    fun combineSolutions(pins1: Set<String>, pins2: Set<String>): Set<String> {
         return pins1.stream()
-                .flatMap((pin1) ->
-                        pins2
-                                .stream()
-                                .map((pin2) -> pin1 + pin2))
-                .collect(Collectors.toSet());
+            .flatMap { pin1: String ->
+                pins2
+                    .stream()
+                    .map { pin2: String -> pin1 + pin2 }
+            }
+            .collect(Collectors.toSet())
     }
 }
